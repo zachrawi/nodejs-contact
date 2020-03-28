d7a3d26b0f1512be3a3580dfb239866d32547eb6
Zachrawi Al Farizi <zachrawi@ngoprekweb.com>
2020-03-28 16:38:25 +0700

    Perbaikan untuk heroku

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/contact', (req, res) => {
     res.send(db.loadContacts());
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server berhasil jalan di port ${port}`);
 });
